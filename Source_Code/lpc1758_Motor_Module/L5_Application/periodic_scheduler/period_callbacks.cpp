d237a6bf4f8dfd951a043c69e6545205bd012d47
Krishna Sai Anirudh Katamreddy <krishnasaianirudh.katamreddy@sjsu.edu>
2017-11-12 21:08:38 -0800

    Updated Motor module, to receive CAN msgs and act
    Receiving CAN msgs from master module, run the motors as per the received msgs

--- Source_Code/lpc1758_Motor_Module/L5_Application/periodic_scheduler/period_callbacks.cpp
+++ Source_Code/lpc1758_Motor_Module/L5_Application/periodic_scheduler/period_callbacks.cpp
@@ -15,7 +15,6 @@
  *     You can reach the author of this software at :
  *          p r e e t . w i k i @ g m a i l . c o m
  */
-
 /**
  * @file
  * This contains the period callback functions for the periodic scheduler
@@ -27,25 +26,22 @@
  * For example, the 1000Hz take slot runs periodically every 1ms, and whatever you
  * do must be completed within 1ms.  Running over the time slot will reset the system.
  */
-
 #include <stdint.h>
-#include "printf_lib.h"
+#include <stdio.h>
 #include "io.hpp"
 #include "periodic_callback.h"
+#include "uart3.hpp"
+#include "can.h"
+#include "printf_lib.h"
+#include "_can_dbc/generated_can.h"
 #include "lpc_pwm.hpp"
-#include "stdio.h"
 #include "gpio.hpp"
 #include "utilities.h"
-#include "can.h"
-#include "motors_module.h"
-#include "_can_dbc/generated_can.h"
-
-const uint32_t                             MOTOR_SIGNAL__MIA_MS = 0;
-const MOTOR_SIGNAL_t                       MOTOR_SIGNAL__MIA_MSG = {0};     // Change default values later.
-
+#include "stdio.h"
+PWM servo_motor(PWM::pwm1, 9.915); //servo motor on P2.0 and 50Hz.
+PWM DC_motor(PWM::pwm2, 100); //servo motor on P2.0 and 50Hz
 /// This is the stack size used for each of the period tasks (1Hz, 10Hz, 100Hz, and 1000Hz)
 const uint32_t PERIOD_TASKS_STACK_SIZE_BYTES = (512 * 4);
-
 /**
  * This is the stack size of the dispatcher task that triggers the period tasks to run.
  * Minimum 1500 bytes are needed in order to write a debug file if the period tasks overrun.
@@ -53,77 +49,135 @@ const uint32_t PERIOD_TASKS_STACK_SIZE_BYTES = (512 * 4);
  * printf inside these functions, you need about 1500 bytes minimum
  */
 const uint32_t PERIOD_MONITOR_TASK_STACK_SIZE_BYTES = (512 * 3);
-
+//MOTOR_STATUS_T create_struct(uint8_t error, float speed, uint32_t isMia, uint32_t miaCountMS)
+//{
+// MOTOR_STATUS_t motor_status1;
+// motor_status1.MOTOR_STATUS_speed_kph = speed;
+// motor_status1.MOTOR_STATUS_wheel_error = error;
+// motor_status1.mia_info.is_mia = isMia;
+// motor_status1.mia_info.mia_counter_ms = miaCountMS;
+//
+// return motor_status1;
+//}
+const uint32_t MOTOR_SIGNAL__MIA_MS = 3000;
+const MOTOR_SIGNAL_t MOTOR_SIGNAL__MIA_MSG = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0};
+MOTOR_SIGNAL_t motor_signal;
+can_t can_1 = can1;
+//Uart3 &u3 = Uart3::getInstance();
 /// Called once before the RTOS is started, this is a good place to initialize things once
 bool period_init(void)
 {
-    CAN_init(can2, 100, 5, 5, NULL, NULL);
-    CAN_bypass_filter_accept_all_msgs();
-    CAN_reset_bus(can2);
+    if(CAN_init(can_1, 100, 80, 80, NULL, NULL))
+    {
+        u0_dbg_printf("init up\n");
+        CAN_reset_bus(can_1);
+        CAN_bypass_filter_accept_all_msgs();
+    }
+    motor_signal.mia_info.is_mia = false;
+    motor_signal.mia_info.mia_counter_ms = 0;
+    // MOTOR_STATUS__MIA_MSG.MOTOR_STATUS_speed_kph = 115.6;
+    // MOTOR_STATUS__MIA_MSG.MOTOR_STATUS_wheel_error = 0;
+    // MOTOR_STATUS__MIA_MSG.mia_info.is_mia = true;
+    // MOTOR_STATUS__MIA_MSG.mia_info.mia_counter_ms = 3000;
     return true; // Must return true upon success
+    //u3.init(38400, 500, 500);
 }
-
 /// Register any telemetry variables
 bool period_reg_tlm(void)
 {
     // Make sure "SYS_CFG_ENABLE_TLM" is enabled at sys_config.h to use Telemetry
     return true; // Must return true upon success
 }
-
-
-
+/**
+ * Below are your periodic functions.
+ * The argument 'count' is the number of times each periodic task is called.
+ */
+int k = 0;
 void period_1Hz(uint32_t count)
 {
-    //LE.toggle(1);
-    if (CAN_is_bus_off(can2))
-        CAN_reset_bus(can2);
+    if(CAN_is_bus_off(can_1))
+    {
+        CAN_reset_bus(can_1);
+        u0_dbg_printf("buss is off, just reseted! \n");
+    }
+    //LE.off(1);
 }
-
 void period_10Hz(uint32_t count)
 {
-    MOTOR_SIGNAL_t motor_data_received = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
-    can_msg_t message = {0};
-    CAN_rx(can2, &message, 10);
-    LE.set(1, 0);
-    LE.set(2, 0);
-    LE.set(3, 0);
-    LE.set(4, 0);
-
-    dbc_msg_hdr_t header_motors = {0};
-
-    header_motors.mid = message.msg_id;
-    header_motors.dlc = message.frame_fields.data_len;
-    dbc_decode_MOTOR_SIGNAL(&motor_data_received, message.data.bytes, &header_motors);
-
-    //dbc_handle_mia_MOTOR_SIGNAL(&motor_data_received, 1);
-
-
-    if(motor_data_received.MOTOR_STEER_FULL_LEFT)
-        LE.set(1, servo_motor_steer_angle (left_dir, motor_data_received.MOTOR_STEER_ANGLE));
-    else if(motor_data_received.MOTOR_STEER_FULL_RIGHT)
-        LE.set(2, servo_motor_steer_angle (right_dir, motor_data_received.MOTOR_STEER_ANGLE));
-    else
-        servo_motor_steer_angle (straight_dir, motor_data_received.MOTOR_STEER_ANGLE);
-
-
-    if(motor_data_received.MOTOR_DRIVE_FORWARD)
-        LE.set(3, dc_motor_drive (forward_drive, motor_data_received.MOTOR_DRIVE_SPEED));
-    else if(motor_data_received.MOTOR_DRIVE_REVERSE)
-        LE.set(4, dc_motor_drive (reverse_drive, motor_data_received.MOTOR_DRIVE_SPEED));
-    else
-    	dc_motor_drive (neutral_drive, motor_data_received.MOTOR_DRIVE_SPEED);
+    // if(k == 1)
+    // {
+    //  DC_motor.set(16);
+    // }
+    // else if(k == 2)
+    // {
+    //  LE.toggle(3);
+    //  DC_motor.set(15.3);
+    //  k = 2;
+    // }
 }
-
 void period_100Hz(uint32_t count)
 {
-    //LE.toggle(3);
-
+    can_msg_t can_msg;
+    if(CAN_rx(can_1, &can_msg, 0))
+    {
+        dbc_msg_hdr_t msg_hdr;
+        msg_hdr.dlc = can_msg.frame_fields.data_len;
+        msg_hdr.mid = can_msg.msg_id;
+        LE.toggle(2);
+        dbc_decode_MOTOR_SIGNAL(&motor_signal, can_msg.data.bytes, &msg_hdr);
+    }
+    else if(dbc_handle_mia_MOTOR_SIGNAL(&motor_signal, 10))
+    {
+        if(motor_signal.MOTOR_DRIVE_REVERSE == 1)
+        {
+            LD.setLeftDigit(1);
+        }
+        LE.toggle(4);
+    }
+    // dc motor
+    
+    if(motor_signal.MOTOR_DRIVE_SPEED == 0)
+    {
+        DC_motor.set(10.1);
+    }
+    else if(motor_signal.MOTOR_DRIVE_SPEED == 5)
+    {
+        DC_motor.set(17.16);
+    }
+    else if(motor_signal.MOTOR_DRIVE_SPEED == 15)
+    {
+        DC_motor.set(17.26);
+    }
+    else if(motor_signal.MOTOR_DRIVE_SPEED == 25)
+    {
+        DC_motor.set(17.33);
+    }
+    
+    //servo motor
+    if(motor_signal.MOTOR_STEER_FULL_LEFT)
+    {
+        servo_motor.set(10.1);
+    }
+    else if(motor_signal.MOTOR_STEER_FULL_RIGHT)
+    {
+        servo_motor.set(19.8);
+    }
+    else if(motor_signal.MOTOR_STEER_STRAIGHT)
+    {
+        servo_motor.set(15.4);
+    }
+    else if(motor_signal.MOTOR_STEER_SLIGHT_LEFT)
+    {
+        servo_motor.set(13.4);
+    }
+    else if(motor_signal.MOTOR_STEER_SLIGHT_RIGHT)
+    {
+        servo_motor.set(17.4);
+    }
 }
 
 // 1Khz (1ms) is only run if Periodic Dispatcher was configured to run it at main():
 // scheduler_add_task(new periodicSchedulerTask(run_1Khz = true));
 void period_1000Hz(uint32_t count)
 {
-    //LE.on(4);
 }
