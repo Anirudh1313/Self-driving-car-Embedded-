c6687c7af66119872bc6601fe04cade8c0e2361d
Krishna Sai Anirudh Katamreddy <krishnasaianirudh.katamreddy@sjsu.edu>
2017-11-12 21:13:02 -0800

    Updated master module, obstacle avoidance algo.
    Getting the front three sensor reading and setting up the values for the parameters to be sent in the can messages to the motor module.

--- Source_Code/lpc1758_Master_Module/L5_Application/periodic_scheduler/period_callbacks.cpp
+++ Source_Code/lpc1758_Master_Module/L5_Application/periodic_scheduler/period_callbacks.cpp
@@ -60,16 +60,16 @@ MOTOR_SIGNAL_t obstacle_avoidance(SENSOR_SONARS_t sensor_msg_decoded);
 /// Called once before the RTOS is started, this is a good place to initialize things once
 bool period_init(void)
 {
-	//initializing the CAN bus
-	CAN_init(can2, 100, 5, 5, nullptr, nullptr);
-	//if receiving enable this code
-	CAN_bypass_filter_accept_all_msgs();
-	//resetting
-	CAN_reset_bus(can2);
-
-	//To be removed after integration
-	SW_obj.init();
-	return true; // Must return true upon success
+    //initializing the CAN bus
+    CAN_init(can1, 100, 5, 5, nullptr, nullptr);
+    //if receiving enable this code
+    CAN_bypass_filter_accept_all_msgs();
+    //resetting
+    CAN_reset_bus(can1);
+    
+    //To be removed after integration
+    SW_obj.init();
+    return true; // Must return true upon success
 }
 
 /// Register any telemetry variables
@@ -86,214 +86,286 @@ bool period_reg_tlm(void)
 
 void period_1Hz(uint32_t count)
 {
-	if(CAN_is_bus_off(can2))
-		CAN_reset_bus(can2);
+    if(CAN_is_bus_off(can1))
+        CAN_reset_bus(can1);
 }
 
 bool dbc_app_send_can_msg(uint32_t mid, uint8_t dlc, uint8_t bytes[8])
 {
-	can_msg_t msg;
-	msg.msg_id = mid;
-	msg.frame_fields.data_len = dlc;
-	msg.frame_fields.is_29bit = 0;
-	msg.frame_fields.is_rtr = 0;
-
-	memcpy(msg.data.bytes, bytes, dlc);
-
-	return CAN_tx(can2, &msg , 10);
-
+    can_msg_t msg;
+    msg.msg_id = mid;
+    msg.frame_fields.data_len = dlc;
+    msg.frame_fields.is_29bit = 0;
+    msg.frame_fields.is_rtr = 0;
+    
+    memcpy(msg.data.bytes, bytes, dlc);
+    
+    return CAN_tx(can1, &msg , 10);
+    
 }
 void period_10Hz(uint32_t count)
 {
-
+    
 }
 
 void period_100Hz(uint32_t count)
- {
-	can_msg_t message_recd;
-	if (!CAN_is_bus_off(can2)) {
-
-		//Receive Sensor Data
-		if(CAN_rx(can2, &message_recd, 0))
-		{
-			dbc_msg_hdr_t can_msg_hdr;
-			can_msg_hdr.dlc = message_recd.frame_fields.data_len;
-			can_msg_hdr.mid = message_recd.msg_id;
-			dbc_decode_SENSOR_SONARS(&sensor_msg_decoded, message_recd.data.bytes,&can_msg_hdr);
-			u0_dbg_printf("%d\n",sensor_msg_decoded.SENSOR_SONARS_left); //Delete this later
-			LE.set(1,0);
-		}
-		else if(dbc_handle_mia_SENSOR_SONARS(&sensor_msg_decoded, 10))
-		{
-			LE.set(1,1);
-			u0_dbg_printf("M ");
-		}
-		//Delete this bit
-		/*sensor_msg_decoded.SENSOR_SONARS_left = 130;
-		sensor_msg_decoded.SENSOR_SONARS_middle = 25;
-		sensor_msg_decoded.SENSOR_SONARS_right = 25;
-		*/
-		//Call for obstacle avoidance
-		MOTOR_SIGNAL_t motor_msg_to_send = { 0 };
-		motor_msg_to_send = obstacle_avoidance(sensor_msg_decoded);
-
-		//Send Motor Data : Sample code for motor testing left right
-		//Comment it to test obstacle avoidance
-		/*
-		 MOTOR_SIGNAL_t motor_msg_to_send = { 0 };
-
-		 if(SW_obj.getSwitch(1))
-		 {
-		 motor_msg_to_send.MOTOR_STEER_ANGLE = 10.1;
-		 motor_msg_to_send.MOTOR_STEER_FULL_LEFT = 1;
-		 }
-		 else if(SW_obj.getSwitch(2))
-		 {
-		 motor_msg_to_send.MOTOR_STEER_ANGLE = 19.8;
-		 motor_msg_to_send.MOTOR_STEER_FULL_RIGHT = 1;
-		 }
-		 else
-		 {
-		 motor_msg_to_send.MOTOR_STEER_ANGLE = 15.1;
-		 motor_msg_to_send.MOTOR_STEER_STRAIGHT = 1;
-		 }
-
-		 if(SW_obj.getSwitch(3))
-		 {
-		 motor_msg_to_send.MOTOR_DRIVE_SPEED = 15;
-		 motor_msg_to_send.MOTOR_DRIVE_FORWARD = 1;
-		 u0_dbg_printf("forward");
-		 }
-		 else if(SW_obj.getSwitch(4))
-		 {
-		 motor_msg_to_send.MOTOR_DRIVE_SPEED = 10;
-		 motor_msg_to_send.MOTOR_DRIVE_REVERSE = 1;
-		 }
-		 else
-		 {
-		 motor_msg_to_send.MOTOR_DRIVE_SPEED = 20;
-		 motor_msg_to_send.MOTOR_DRIVE_NEUTRAL = 1;
-		 }
-		 */
-
-		dbc_encode_and_send_MOTOR_SIGNAL(&motor_msg_to_send);
-	} else {
-		CAN_reset_bus(can2);
-	}
-
-	// LE.toggle(3);
+{
+    can_msg_t message_recd;
+    if (!CAN_is_bus_off(can1)) {
+        
+        //Receive Sensor Data
+        if(CAN_rx(can1, &message_recd, 0))
+        {
+            dbc_msg_hdr_t can_msg_hdr;
+            can_msg_hdr.dlc = message_recd.frame_fields.data_len;
+            can_msg_hdr.mid = message_recd.msg_id;
+            dbc_decode_SENSOR_SONARS(&sensor_msg_decoded, message_recd.data.bytes,&can_msg_hdr);
+            //u0_dbg_printf("%d\n",sensor_msg_decoded.SENSOR_SONARS_left); //Delete this later
+            LE.set(1,0);
+        }
+        else if(dbc_handle_mia_SENSOR_SONARS(&sensor_msg_decoded, 10))
+        {
+            LE.set(1,1);
+            u0_dbg_printf("MIA ");
+        }
+        //Delete this bit
+        /*sensor_msg_decoded.SENSOR_SONARS_left = 130;
+         sensor_msg_decoded.SENSOR_SONARS_middle = 25;
+         sensor_msg_decoded.SENSOR_SONARS_right = 25;
+         */
+        //Call for obstacle avoidance
+        MOTOR_SIGNAL_t motor_msg_to_send = { 0 };
+        motor_msg_to_send = obstacle_avoidance(sensor_msg_decoded);
+        
+        dbc_encode_and_send_MOTOR_SIGNAL(&motor_msg_to_send);
+    }
+    else
+    {
+        CAN_reset_bus(can1);
+    }
+    
+    // LE.toggle(3);
 }
 
 // 1Khz (1ms) is only run if Periodic Dispatcher was configured to run it at main():
 // scheduler_add_task(new periodicSchedulerTask(run_1Khz = true));
 void period_1000Hz(uint32_t count)
 {
-  //  LE.toggle(4);
+    //  LE.toggle(4);
 }
 
+static int spC;
+static int spM;
 
 //Obstacle avoidance algorithm
 MOTOR_SIGNAL_t obstacle_avoidance(SENSOR_SONARS_t sensor_msg_decoded)
 {
-	MOTOR_SIGNAL_t obst_avoided_msg = { 0 };
-
-	/* Ranges: 	Stop range (0): < 20 inches
-	 * 			FullLeft/FullRight range (1): 21 - 110 inches
-	 * 			SlightLeft/SlightRiht range (2): 111 - 200 inches
-	 */
-	uint16_t left_range;
-	uint16_t middle_range ;
-	uint16_t right_range ;
-	//uint16_t rear_range = 0;
-
-	//left sensor range calculation
-	        if(sensor_msg_decoded.SENSOR_SONARS_left<=20)
-	        			left_range = 0;
-	        else if(sensor_msg_decoded.SENSOR_SONARS_left>20 && sensor_msg_decoded.SENSOR_SONARS_left<=110)
-	        			left_range = 1;
-	        else
-	        			left_range = 2;
-
-	//middle sensor range calculation
-			if(sensor_msg_decoded.SENSOR_SONARS_middle<=20)
-					middle_range = 0;
-			else if(sensor_msg_decoded.SENSOR_SONARS_middle>20 && sensor_msg_decoded.SENSOR_SONARS_middle<=110)
-					middle_range = 1;
-			else
-					middle_range = 2;
-
-	//right sensor range calculation
-			if(sensor_msg_decoded.SENSOR_SONARS_right<=20)
-					right_range = 0;
-			else if(sensor_msg_decoded.SENSOR_SONARS_right>20 && sensor_msg_decoded.SENSOR_SONARS_right<=110)
-					right_range = 1;
-			else
-					right_range = 2;
-
-
-	u0_dbg_printf("%d %d %d",left_range,middle_range,right_range);
-
-	//Stop 000
-	if(left_range == 0  && middle_range == 0 && right_range == 0)
-	{
-		//Stop
-		obst_avoided_msg.MOTOR_DRIVE_NEUTRAL = 1;
-		obst_avoided_msg.MOTOR_STEER_STRAIGHT = 1;
-		obst_avoided_msg.MOTOR_STEER_ANGLE = 15.1;
-		obst_avoided_msg.MOTOR_DRIVE_SPEED = 0;
-		u0_dbg_printf("STP1\n"); //Delete this later
-	}
-	//Stop - 001/100
-	else if((left_range == 0  && middle_range == 0 && right_range == 1)
-			|| ((left_range == 1  && middle_range == 0 && right_range == 0)))
-	{
-		//Stop
-		obst_avoided_msg.MOTOR_DRIVE_NEUTRAL = 1;
-		obst_avoided_msg.MOTOR_STEER_STRAIGHT = 1;
-		obst_avoided_msg.MOTOR_STEER_ANGLE = 15.1;
-		obst_avoided_msg.MOTOR_DRIVE_SPEED = 0;
-		u0_dbg_printf("STP2\n"); //Delete this later
-	}
-	//Half Right forward -011/012/021/022
-	else if(left_range == 0  && (middle_range == 1 || middle_range == 2)
-			&& (right_range == 1 || right_range == 2))
-	{
-		obst_avoided_msg.MOTOR_DRIVE_FORWARD = 1;
-		obst_avoided_msg.MOTOR_STEER_FULL_RIGHT = 1;
-		obst_avoided_msg.MOTOR_STEER_ANGLE = 17.5;
-		obst_avoided_msg.MOTOR_DRIVE_SPEED = 15;
-		u0_dbg_printf("RF\n"); //Delete this later
-	}
-	//Half Left forward - 110/120/210/220
-	else if((left_range == 1 || left_range == 2)  && (middle_range == 1 || middle_range == 2)
-			&& right_range == 0)
-	{
-		obst_avoided_msg.MOTOR_DRIVE_FORWARD = 1;
-		obst_avoided_msg.MOTOR_STEER_FULL_LEFT = 1;
-		obst_avoided_msg.MOTOR_STEER_ANGLE = 12.6;
-		obst_avoided_msg.MOTOR_DRIVE_SPEED = 15;
-		u0_dbg_printf("LF\n"); //Delete this later
-	}
-	//Straight - 010/020
-	else if(left_range == 0  && (middle_range == 1 || middle_range == 2)
-				&& right_range == 0)
-	{
-		obst_avoided_msg.MOTOR_DRIVE_FORWARD = 1;
-		obst_avoided_msg.MOTOR_STEER_STRAIGHT = 1;
-		obst_avoided_msg.MOTOR_STEER_ANGLE = 15.1;
-		obst_avoided_msg.MOTOR_DRIVE_SPEED = 15;
-		u0_dbg_printf("SFW\n"); //Delete this later
-
-	}
-	//Default condition - should be follow GPS, for now we have default go straight
-	else
-	{
-		obst_avoided_msg.MOTOR_DRIVE_FORWARD = 1;
-		obst_avoided_msg.MOTOR_STEER_STRAIGHT = 1;
-		obst_avoided_msg.MOTOR_STEER_ANGLE = 15.1;
-		obst_avoided_msg.MOTOR_DRIVE_SPEED = 15;
-		u0_dbg_printf("D\n"); //Delete this later
-	}
-
-	return obst_avoided_msg;
+    MOTOR_SIGNAL_t obst_avoided_msg = { 0 };
+    
+    /* Ranges:     Stop range (0): < 20 inches
+     *             FullLeft/FullRight range (1): 21 - 110 inches
+     *             SlightLeft/SlightRiht range (2): 111 - 200 inches
+     */
+    uint16_t left_range = 0;
+    uint16_t middle_range = 0 ;
+    uint16_t right_range = 0 ;
+    //uint16_t rear_range = 0;
+    
+    //left sensor range calculation
+    if(sensor_msg_decoded.SENSOR_SONARS_left>0 && sensor_msg_decoded.SENSOR_SONARS_left<=25)
+    {
+        left_range = -1;
+    }
+    else if(sensor_msg_decoded.SENSOR_SONARS_left>20 && sensor_msg_decoded.SENSOR_SONARS_left<=45)
+    {
+        left_range = 0;
+    }
+    else if(sensor_msg_decoded.SENSOR_SONARS_left>45 && sensor_msg_decoded.SENSOR_SONARS_left<=110)
+    {
+        left_range = 1;
+    }
+    else if(sensor_msg_decoded.SENSOR_SONARS_left > 110)
+    {
+        left_range = 2;
+    }
+    
+    //middle sensor range calculation
+    if(sensor_msg_decoded.SENSOR_SONARS_middle <= 40 && sensor_msg_decoded.SENSOR_SONARS_middle >= 0)
+    {
+        middle_range = 0;
+    }
+    else if(sensor_msg_decoded.SENSOR_SONARS_middle > 40 && sensor_msg_decoded.SENSOR_SONARS_middle <= 80)
+    {
+        middle_range = 1;
+    }
+    else if(sensor_msg_decoded.SENSOR_SONARS_middle > 80 && sensor_msg_decoded.SENSOR_SONARS_middle <= 180)
+    {
+        middle_range = 2;
+    }
+    else if(sensor_msg_decoded.SENSOR_SONARS_middle > 180)
+    {
+        middle_range = 3;
+    }
+    
+    //right sensor range calculation
+    if(sensor_msg_decoded.SENSOR_SONARS_right>0 && sensor_msg_decoded.SENSOR_SONARS_right<=25)
+    {
+        right_range = -1;
+    }
+    else if(sensor_msg_decoded.SENSOR_SONARS_right>20 && sensor_msg_decoded.SENSOR_SONARS_right<=45)
+    {
+        right_range = 0;
+    }
+    else if(sensor_msg_decoded.SENSOR_SONARS_right> 45 && sensor_msg_decoded.SENSOR_SONARS_right<=110)
+    {
+        right_range = 1;
+    }
+    else if(sensor_msg_decoded.SENSOR_SONARS_right > 110)
+    {
+        right_range = 2;
+    }
+    
+    
+    
+    
+    //Stop 000
+    if(middle_range == 0)
+    {
+        //Stop
+        //set bit, middle range = 3, the bit set to 1.
+        spC = 0;
+        spM = 0;   //this bit is set to 1, middle-range = 2
+        
+        obst_avoided_msg.MOTOR_STEER_STRAIGHT = 1;
+        obst_avoided_msg.MOTOR_DRIVE_SPEED = 0;
+        u0_dbg_printf(" 0 ");
+        
+    }
+    else if(middle_range == 1)
+    {    u0_dbg_printf(" M1 ");
+        if(spC == 1)
+        {
+            obst_avoided_msg.MOTOR_DRIVE_SPEED = 0;
+            spC = 0;
+        }
+        else if(spM == 1)
+        {
+            obst_avoided_msg.MOTOR_DRIVE_SPEED = 5;
+            spM = 0;
+        }
+        else
+        {
+            obst_avoided_msg.MOTOR_DRIVE_SPEED = 5;
+            
+            if(left_range == 0 || left_range == -1)
+            {
+                obst_avoided_msg.MOTOR_STEER_FULL_RIGHT = 1;
+            }
+            else if(right_range == 0 || right_range == -1)
+            {
+                obst_avoided_msg.MOTOR_STEER_FULL_LEFT = 1;
+            }
+            else
+            {
+                obst_avoided_msg.MOTOR_STEER_STRAIGHT = 1;
+            }
+        }
+        
+    }
+    else if(middle_range == 2)
+    {    u0_dbg_printf(" M2 ");
+        if(spC == 1)
+        {
+            
+            obst_avoided_msg.MOTOR_DRIVE_SPEED = 5;
+            spC = 0;
+        }
+        else
+        {
+            
+            if(left_range == -1 && right_range == -1)
+            {
+                obst_avoided_msg.MOTOR_STEER_STRAIGHT = 1;
+                obst_avoided_msg.MOTOR_DRIVE_SPEED = 0;
+            }
+            else if(left_range == 0 && right_range == 0)
+            {
+                obst_avoided_msg.MOTOR_STEER_STRAIGHT = 1;
+                obst_avoided_msg.MOTOR_DRIVE_SPEED = 5;
+            }
+            else if((left_range == 0 && right_range == -1) || (left_range == -1 && right_range == 0))
+            {
+                obst_avoided_msg.MOTOR_STEER_STRAIGHT = 1;
+                obst_avoided_msg.MOTOR_DRIVE_SPEED = 0;
+            }
+            else if(left_range == 0 || left_range == -1)
+            {
+                obst_avoided_msg.MOTOR_STEER_SLIGHT_RIGHT = 1;
+                obst_avoided_msg.MOTOR_DRIVE_SPEED = 5;
+                
+            }
+            else if(right_range == 0 || right_range == -1)
+            {
+                obst_avoided_msg.MOTOR_STEER_SLIGHT_LEFT = 1;
+                obst_avoided_msg.MOTOR_DRIVE_SPEED = 5;
+                
+            }
+            else
+            {
+                obst_avoided_msg.MOTOR_DRIVE_SPEED = 15;
+                obst_avoided_msg.MOTOR_STEER_STRAIGHT = 1;
+            }
+        }
+        spM = 1;
+    }
+    else if(middle_range == 3)
+    {
+        u0_dbg_printf(" M3 ");
+        
+        if(left_range == -1 && right_range == -1)
+        {
+            obst_avoided_msg.MOTOR_STEER_STRAIGHT = 1;
+            obst_avoided_msg.MOTOR_DRIVE_SPEED = 0;
+        }
+        else if(left_range == 0 && right_range == 0)
+        {
+            obst_avoided_msg.MOTOR_STEER_STRAIGHT = 1;
+            obst_avoided_msg.MOTOR_DRIVE_SPEED = 5;
+        }
+        else if((left_range == 0 && right_range == -1) || (left_range == -1 && right_range == 0))
+        {
+            obst_avoided_msg.MOTOR_STEER_STRAIGHT = 1;
+            obst_avoided_msg.MOTOR_DRIVE_SPEED = 0;
+        }
+        else if(left_range == -1)
+        {
+            obst_avoided_msg.MOTOR_STEER_FULL_RIGHT = 1;
+            obst_avoided_msg.MOTOR_DRIVE_SPEED = 5;
+        }
+        else if(right_range == -1)
+        {
+            obst_avoided_msg.MOTOR_STEER_FULL_LEFT = 1;
+            obst_avoided_msg.MOTOR_DRIVE_SPEED = 5;
+        }
+        else if(right_range == 0)
+        {
+            obst_avoided_msg.MOTOR_STEER_SLIGHT_LEFT = 1;
+            obst_avoided_msg.MOTOR_DRIVE_SPEED = 5;//later make 15
+        }
+        else if(left_range == 0)
+        {
+            obst_avoided_msg.MOTOR_STEER_SLIGHT_RIGHT = 1;
+            obst_avoided_msg.MOTOR_DRIVE_SPEED = 5;//later make 15
+        }
+        else
+        {
+            obst_avoided_msg.MOTOR_DRIVE_SPEED = 15;  // make 25 on testing
+            obst_avoided_msg.MOTOR_STEER_STRAIGHT = 1;
+            //This block dteer the CAR according to GPS coordinates
+        }
+        spC = 1;
+    }
+    
+    
+    return obst_avoided_msg;
 }
