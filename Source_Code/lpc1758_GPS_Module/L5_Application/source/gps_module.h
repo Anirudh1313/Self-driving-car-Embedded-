85d0e321a0effc96b4a682aa083d827effb4a1a7
Krishna Sai Anirudh Katamreddy <krishnasaianirudh.katamreddy@sjsu.edu>
2018-03-25 18:01:47 -0700

    turn_direction

--- Source_Code/lpc1758_GPS_Module/L5_Application/source/gps_module.h
+++ Source_Code/lpc1758_GPS_Module/L5_Application/source/gps_module.h
@@ -2,19 +2,19 @@
  * gps_module.h
  *
  *  Created on: Oct 31, 2017
- *      Author: sushm
+ *      Author: Krish
  */
 
 #ifndef L5_APPLICATION_SOURCE_GPS_MODULE_H_
 #define L5_APPLICATION_SOURCE_GPS_MODULE_H_
 
-float bearing_angle(float lat1, float long1, float lat2, float long2);
+#include <stdint.h>
 
-float heading_angle(float x, float y, float z);
+float bearing_angle(float lat1, float long1, float lat2, float long2);
 
 float distance(float lat1, float long1, float lat2, float long2);
 
-
+uint8_t turn_direction(float degree);
 
 
 #endif /* L5_APPLICATION_SOURCE_GPS_MODULE_H_ */
